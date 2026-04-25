cc9dd3362a6a00da64c730217100945477425380
albertocll <albertoclaroslaboral@gmail.com>
2026-04-25 07:53:41 +0200

    fix: mejora del joystick

--- Assets/Script/UI/Joystick.cs
+++ Assets/Script/UI/Joystick.cs
@@ -4,20 +4,35 @@ using UnityEngine.EventSystems;
 public class Joystick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
 {
     [SerializeField] private RectTransform handle;
-    [SerializeField] private float range = 50f;
+    [SerializeField] private RectTransform joystickContainer;
+    [SerializeField] private float range = 100f;
 
+    private Canvas canvas;
     public Vector2 Direction { get; private set; }
 
+    private void Awake()
+    {
+        canvas = GetComponentInParent<Canvas>();
+        joystickContainer.gameObject.SetActive(false);
+    }
     public void OnPointerDown(PointerEventData eventData)
     {
-        OnDrag(eventData);
+        joystickContainer.gameObject.SetActive(true);
+
+        Vector2 pos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            canvas.GetComponent<RectTransform>(), eventData.position, eventData.pressEventCamera, out pos);
+        joystickContainer.anchoredPosition = pos;
+
+        handle.anchoredPosition = Vector2.zero;
+        Direction = Vector2.zero;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         Vector2 pos;
         RectTransformUtility.ScreenPointToLocalPointInRectangle(
-            GetComponent<RectTransform>(), eventData.position, eventData.pressEventCamera, out pos);
+            joystickContainer, eventData.position, eventData.pressEventCamera, out pos);
 
         pos = Vector2.ClampMagnitude(pos, range);
         handle.anchoredPosition = pos;
@@ -26,6 +41,7 @@ public class Joystick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPoint
 
     public void OnPointerUp(PointerEventData eventData)
     {
+        joystickContainer.gameObject.SetActive(false);
         handle.anchoredPosition = Vector2.zero;
         Direction = Vector2.zero;
     }
